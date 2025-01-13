cd5aba7eb03f0d21b3dd1cde16ddbe36ae1625a5
Madamgearbox <madamgearbox@gmail.com>
2025-01-12 20:54:22 -0500

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -1,2 +1,58 @@
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("description");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("time");
+    let date = new Date(response.data,time * 1000);
+    
+
+    cityElement.innerHTML = response.data.city;
+    timeElement.innerHTML = formatDate(date);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `$(response.data.temperature.humidity)$`;
+    windSpeedElement.innerHTML = `$(response.data.wind.speed)km/h`;
+    temperatureElement.innerHTML = Math.round(temperature);
+
+}
+
+function formatDate(date) {
+    let day = date.getDay();
+    let minuets = date.getMinuets();
+    let hours = date.getHours();
+    let days = (
+        "Sunday",
+        "Monday", 
+        "Tuesday", 
+        "Wednesday", 
+        "Thursday", 
+        "Friday", 
+        "Saturday"
+    );
+    let day = days[date.getDay()]; 
+
+    if (minuets < 10) {
+        minuets = `0${minuets}`;
+    }
+
+    return `${day} ${hours}:${minuets}`;
+}
+
+function searchCity(city) {
+ let apiKey = "012b5b3a44t6b0bodaf08aa43444f3f6";
+ let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+ axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSearchSubmit(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+   
+    searchCity(searchInput.value);
+}
+
 let searchFormElement = document.querySelector("#search-form");
-console.log(searchFormElement);
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Lisbon");
